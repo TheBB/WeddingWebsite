bb775903ba75d5b73012570b54363baeeffacafc
Eivind Fonn <evfonn@gmail.com>
2018-07-22 21:44:44 +0200

    Hello world

---
+++ wedding/__init__.py
@@ -0,0 +1,25 @@
+from flask import render_template, Flask
+import os
+
+
+def index():
+    return render_template('index.djhtml')
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(SECRET_KEY='dev')
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    app.route('/')(index)
+
+    return app
